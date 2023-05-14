cdcd6995443ed4701e11af7fda4cd555961d85dc
Jay <jay@jayonsoftwre.com>
2023-05-14 18:52:04 -0400

    More Code Cleanup

--- Program.cs
+++ Program.cs
@@ -15,27 +15,35 @@ namespace Apex.CUDA
         {
             //BottleNeckTest();
 
-
+            //PrintGpuDetails();
+            
             Console.WriteLine("{0,-20} {1,-20} {2,-20} {3,-20} {4,-20}\n", "Length", "SingleThread", "TPL", "CLA", "CUDA");
             //TestStart(10);
             //TestStart(1_000);
             //TestStart(10_000);
             //TestStart(100_000);
-            //TestStart(1_000_000);
+            TestStart(1_000_000);
             //TestStart(10_000_000);
-            TestStart(100_000_000);
+            //TestStart(100_000_000);
             //TestStart(1_000_000_000);
             //TestStart(2_146_435_071);
 
             Console.WriteLine("Done, Press any key to exit");
-            Console.ReadKey();
+            //Console.ReadKey();
         }
 
         public static void TestStart(long length)
         {
             var lengthString = length.ToString("#,##0");
+            
+            Context con = Context.Create(builder => builder.AllAccelerators());
 
-            List<AcceleratorType> accTypes = ListGPU();
+            // Get a list of all available accelerators (including the CPU/GPU)
+            List<AcceleratorType> accTypes = new List<AcceleratorType>();
+            foreach (Device device in con)
+            {
+                accTypes.Add(device.AcceleratorType);
+            }
 
             if (accTypes.Contains(AcceleratorType.OpenCL) && accTypes.Contains(AcceleratorType.Cuda))
             {
@@ -55,24 +63,25 @@ namespace Apex.CUDA
             }
         }
 
-        public static List<AcceleratorType> ListGPU()
+        public static void PrintGpuDetails()
         {
-            Context con = Context.Create(builder => builder.AllAccelerators());
-
-            // Get a list of all available accelerators (including the CPU)
-            // CPUAccelerator[Type: CPU, WarpSize: 4, MaxNumThreadsPerGroup: 16, MemorySize: 9223372036854775807]
-            // Intel(R) HD Graphics 630[Type: OpenCL, WarpSize: 0, MaxNumThreadsPerGroup: 256, MemorySize: 13557809152]
-            // NVIDIA GeForce GTX 1050[Type: Cuda, WarpSize: 32, MaxNumThreadsPerGroup: 1024, MemorySize: 4294836224]
-
-            List<AcceleratorType> acceleratorTypes = new List<AcceleratorType>();
-            foreach (Device device in con)
+            Context acceleratorContext = Context.Create(builder => builder.AllAccelerators());
+            
+            foreach (var accelerator in acceleratorContext)
             {
-                acceleratorTypes.Add(device.AcceleratorType);
+                Console.WriteLine($"Name: {accelerator.Name}");
+                Console.WriteLine($"MemorySize: {accelerator.MemorySize}");
+                Console.WriteLine($"MaxThreadsPerGroup: {accelerator.MaxNumThreadsPerGroup}");
+                Console.WriteLine($"MaxSharedMemoryPerGroup: {accelerator.MaxSharedMemoryPerGroup}");
+                Console.WriteLine($"MaxGridSize: {accelerator.MaxGridSize}");
+                Console.WriteLine($"MaxConstantMemory: {accelerator.MaxConstantMemory}");
+                Console.WriteLine($"WarpSize: {accelerator.WarpSize}");
+                Console.WriteLine($"NumMultiprocessors: {accelerator.NumMultiprocessors}");
+                Console.WriteLine();
             }
-
-            return acceleratorTypes;
+            
         }
-
+ 
         public static string CLA(long length)
         {
             String result = "";
@@ -160,50 +169,5 @@ namespace Apex.CUDA
 
             return stopwatch.ElapsedMilliseconds.ToString("#,##0");
         }
-
-
-
-        // Test to see which calls takes the most amount of time
-        public static void BottleNeckTest()
-        {
-
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            long length = 1_000_000_100;
-
-            // Initialize ILGPU
-            Context context = Context.CreateDefault();
-            //Accelerator accelerator = context.CreateCudaAccelerator(0);
-            Accelerator accelerator = context.CreateCLAccelerator(0);
-
-            Console.WriteLine("Initialize ILGPU Took : " + stopwatch.ElapsedTicks.ToString("#,##0"));
-            stopwatch.Restart();
-
-            // Allocate memory on the accelerator.
-            var deviceOutput = accelerator.Allocate1D<int>(length);
-
-            // Load / Compile the kernel. This is where the magic happens.
-            var loadedKernel = accelerator.LoadAutoGroupedStreamKernel(
-            (Index1D i, ArrayView<int> output) =>
-            {
-                output[i] = 5 * 5 * 5 * 5 * 5;
-            });
-
-            Console.WriteLine("Load Kernal Took : " + stopwatch.ElapsedTicks.ToString("#,##0"));
-            stopwatch.Restart();
-
-            // Tell the accelerator to start computing the kernel
-            loadedKernel((int)deviceOutput.Length, deviceOutput.View);
-            Console.WriteLine("Computing Took :" + stopwatch.ElapsedTicks.ToString("#,##0"));
-            stopwatch.Restart();
-
-            // Wait for the accelerator to be finished with whatever it's doing in this case it just waits for the kernel to finish.
-            accelerator.Synchronize();
-            var result = deviceOutput.GetAsArray1D();
-            context.Dispose();
-            Console.WriteLine("Synchronize and Dispose Took :" + stopwatch.ElapsedTicks.ToString("#,##0"));
-        }
-
     }
 }
